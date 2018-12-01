a8f62a42246c4815da47ac3b92830aabda725d58
Adrian Mrówka <34339501+mrowkaadrian@users.noreply.github.com>
2018-12-02 00:02:58 +0100

    Added problem2/ modified description of problem1

--- problem1_1_12.py
+++ problem1_1_12.py
@@ -1,13 +1,20 @@
+# Advent of Code - 2018
+# Problem 1 of 1.12.2018
+#
+# We need to calculate last existing frequency our device will reach. We are using a .txt file, which contains
+#   a number (positive or negative) in every single line. To calculate this, we need to add or subtract number of
+#   each line, starting from value: 0
+
 file = open("problem1_1_12_DATA.txt", "r")
 
 array = []
-final_number = 0
+final_freq = 0
 
 for val in file.read().split():
     array.append(int(val))
 file.close()
 
 for val in array:
-    final_number += val
+    final_freq += val
 
-print(final_number)
+print(final_freq)
---
+++ problem2_1_12.py
@@ -0,0 +1,28 @@
+# Advent of Code - 2018
+# Problem 2 of 1.12.2018
+#
+# We need to detect first frequency our device reaches twice. We are using the same data as in the previous
+#   task.
+
+file = open("problem1_1_12_DATA.txt", "r")
+
+array = []
+search_array = []
+current_freq = 0
+FOUND = 0
+
+for val in file.read().split():
+    array.append(int(val))
+file.close()
+
+while FOUND == 0:
+    for val in array:
+        current_freq += val
+        if (current_freq in search_array):
+            FOUND = 1
+            print(current_freq)
+            break
+        else:
+            search_array.append(current_freq)
+
+print(current_freq)
